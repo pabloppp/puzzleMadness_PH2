4c27eeda80b245da5ca178ddec10dc8547c43c1f
Pablo Pernias <pablo@pernias.com>
2015-05-23 13:37:23 +0200

    casi casi!

--- app.js
+++ app.js
@@ -8,6 +8,40 @@ window.addEventListener("load", function(){ //SE EJECUTA DESPUES DE CARGAR TODO
 	var firstTab = 0;
 	var tabsslides = [];
 	var imgurl;
+	var selectedPiece;
+	var imgPieces = [];
+	var loadedImg;
+	var originalData, puzzleData;
+	var puzzle = [];
+	var movements = 0;
+	var changeCell;
+
+	//WEB WORKER SETUP
+	if(typeof(Worker) !== "undefined") {
+		if(typeof(w) == "undefined") {
+		    w = new Worker("validatorWorker.js");
+		}
+	}
+	else{
+		console.log("WEB WORKERS  NOT SUPPORTED");
+	}
+
+	resetPuzzle = function(){
+		selectedPiece = undefined;
+		imgPieces = [];
+		changeCell = undefined;
+		puzzle = [];
+		for(var i=0;i<6;i++){
+
+			puzzleRow = [];
+			for(var j=0;j<8;j++){
+				puzzleRow.push(undefined);
+			}
+			puzzle.push(puzzleRow);
+
+		}
+	}
+	resetPuzzle();
 
 	var canvas = document.getElementById("puzzle");
 
@@ -35,6 +69,11 @@ window.addEventListener("load", function(){ //SE EJECUTA DESPUES DE CARGAR TODO
 
 	setTab = function(number){
 		if(number == currenttab) return;
+
+		if(number > 0 && !loadedImg){
+			functions().toast("Carga una imagen antes de jugar...", 2000);
+			return;
+		}
 		var tabholder = document.getElementById("tabs");
 		tabs = tabholder.children;
 		if(tabs.length > number){
@@ -97,6 +136,9 @@ window.addEventListener("load", function(){ //SE EJECUTA DESPUES DE CARGAR TODO
 
 	imageloaded = function(img){
 
+		toggleLoading();
+		
+
 		document.getElementById("openbutton").disabled = true;
 		var inputfile = img || document.getElementById("openimage");
 		var imagefile = inputfile.files[0];	
@@ -104,6 +146,7 @@ window.addEventListener("load", function(){ //SE EJECUTA DESPUES DE CARGAR TODO
 
 		if(!imagefile.type.match(/image.*/)){
 			functions().toast("Debes seleccionar una imagen", 5000);
+			toggleLoading();
 			return;
 		}
         else reader.readAsDataURL(imagefile);
@@ -122,14 +165,22 @@ window.addEventListener("load", function(){ //SE EJECUTA DESPUES DE CARGAR TODO
 				canvas.height = image.height;
 				ctx.drawImage(image, 0, 0, image.width, image.height);
 
+				originalData = ctx.getImageData(0, 0, 360, 480);
+
 				document.getElementById("loadedimg").src = canvas.toDataURL();
 				imgurl = canvas.toDataURL(); 
 
 				document.getElementById("openbutton").innerHTML = "CAMBIAR";
-				document.getElementById("instrucciones").innerHTML = "Ahora selecciona la pestaña 'PUZZLE' y... ¡A jugar!";
+				document.getElementById("instrucciones").innerHTML = "<button onclick=\"setTab(1)\" >¡JUGAR!</button>";
 				document.getElementById("openbutton").disabled = false;
-				functions().toast("Imagen cargada con exito", 5000);
+				functions().toast("Imagen cargada con exito", 2000);
+
+				loadedImg = new Image();
+				loadedImg.src = canvas.toDataURL();
+
 				populatePieces();
+
+				toggleLoading();
 
 			}
 			image.src = imgurl;
@@ -147,7 +198,9 @@ window.addEventListener("load", function(){ //SE EJECUTA DESPUES DE CARGAR TODO
 	}
 
 	populatePieces = function(){
-		
+		resetPuzzle();
+		updateCanvas();
+		showhub();
 
 		var container = document.getElementById("piecehub");
 		container.innerHTML = "";		
@@ -156,14 +209,40 @@ window.addEventListener("load", function(){ //SE EJECUTA DESPUES DE CARGAR TODO
 			for(var j=0;j<8;j++){ //X
 				var left = j;
 				var top = i;
-				var piece = document.createElement("IMG");
+				var piece = document.createElement("DIV");
 				piece.setAttribute("draggable", "true");
+				piece.setAttribute("ondragstart", "pieceStart(event)");
+				piece.setAttribute("ondragend", "dragEnded()");
 				piece.style.backgroundImage = "url("+imgurl+")";
 				piece.style.backgroundPosition = "-"+(left*60)+"px -"+(top*60)+"px";
-				container.appendChild(piece);
+				piece.puzzleData = {x:left, y:top};
+				piece.addEventListener("click", function(event){
+					console.log(event);
+					if(selectedPiece == event.target){
+						event.target.classList.remove("selected");	
+						selectedPiece = undefined;
+					}
+					else{
+						if(selectedPiece) selectedPiece.classList.remove("selected");	
+						event.target.classList.add("selected");
+						selectedPiece = event.target;
+						showhub();
+					}
+					changeCell = undefined;
+					updateCanvas();
+				});
+				imgPieces.push(piece);
+				//
 			}
 			
 		}
+
+		//imgPieces.sort(function(){return Math.random() - 0.5})
+
+		for(var i=0;i<imgPieces.length;i++){
+			container.appendChild(imgPieces[i]);
+		}
+		
 	}
 
 	showhub = function(){
@@ -172,6 +251,99 @@ window.addEventListener("load", function(){ //SE EJECUTA DESPUES DE CARGAR TODO
 		button.classList.toggle("active");
 		hub.classList.toggle("active");
 	}
+
+	pieceStart = function(event){
+		//event.preventDefault();
+
+		/*var img = document.createElement("img");
+    	img.src = "http://jerusalemconnection.com/Portals/0/Images/puzzle%20piece%20.png";
+    	event.dataTransfer.setDragImage(img, 0, 0);*/
+		selectedPiece = event.target;
+		changeCell = undefined;
+		showhub();
+	}
+
+	dragEnded = function(event){
+		showhub();
+		selectedPiece = undefined;
+	}
+
+	canvas.ondragover = function () {return false; ev.preventDefault(); };
+	canvas.ondragend = function () {return false; };
+	canvas.ondrop = function (e) {
+		e.preventDefault();
+		addPieza(e, true);	
+	}
+
+	addPieza = function(event, noreshow){
+		if(!loadedImg){
+			functions().toast("Debes cargar una imagen...", 3000);
+			return;	
+		}
+		
+		var ctx = canvas.getContext("2d");	
+		var mouse = getMousePos(canvas, event);
+
+		var posX = Math.floor(mouse.x / 60);
+		var posY = Math.floor(mouse.y / 60);
+
+		if(!selectedPiece){
+
+			if(changeCell){
+
+				var tempPiece = puzzle[posY][posX];
+				puzzle[posY][posX] = puzzle[changeCell.y][changeCell.x];
+				puzzle[changeCell.y][changeCell.x] = tempPiece;
+
+				if(changeCell.x !== posX || changeCell.y !== posY){
+					movements++;
+					document.getElementById("counter").innerHTML = movements;
+				}
+
+				changeCell = undefined;
+
+				updateCanvas();
+
+				return;
+			}
+
+			changeCell = {x:posX, y:posY}
+			updateCanvas();
+			//functions().toast("No has seleccionado ninguna pieza...", 3000);
+			return;
+		}
+
+
+		if( puzzle[posY][posX] ){
+			functions().toast("Ese hueco ya está ocupado", 2000);
+			if(selectedPiece) selectedPiece.classList.remove("selected");	
+			selectedPiece = undefined;
+			updateCanvas();	
+			if(!noreshow) showhub();
+			return;
+		}
+
+		puzzle[posY][posX] = selectedPiece;
+		selectedPiece.parentNode.removeChild(selectedPiece);
+		selectedPiece = undefined;
+		updateCanvas();
+		movements++;
+		document.getElementById("counter").innerHTML = movements;
+		if(!noreshow) showhub();
+	}
+
+	getMousePos = function(canvas, evt){
+		var rect = canvas.getBoundingClientRect();
+
+		var scale = (window.innerWidth-20)/480;
+		if(scale > 1) scale = 1;
+
+		return {
+			x: (evt.clientX - rect.left)/scale,
+			y: (evt.clientY - rect.top)/scale
+		}
+	}
+
 
 	window.addEventListener("resize", function(){
 		canvasresize();
@@ -186,21 +358,33 @@ window.addEventListener("load", function(){ //SE EJECUTA DESPUES DE CARGAR TODO
 		canvas.style.left = (-240*(1-scale))+"px";
 		canvas.style.marginTop = (-190*(1-scale))+"px";
 		canvas.style.marginBottom = (-190*(1-scale))+"px";
+
 	};
 	canvasresize();
 
 	updateCanvas = function(){
 		var ctx = canvas.getContext("2d");
+
+		ctx.clearRect(0, 0, canvas.width, canvas.height);
+
+		for(var y=0;y<6;y++){
+			for(var x=0;x<8;x++){
+				if(puzzle[y][x]){
+					var imgPos = puzzle[y][x].puzzleData;
+					ctx.drawImage(loadedImg, imgPos.x*60, imgPos.y*60, 60, 60, x*60, y*60, 60, 60);
+				}
+			}
+		}
+
+		puzzleData = ctx.getImageData(0, 0, 360, 480);
+
+		ctx.lineWidth = 1;
+
 		for(var y=0;y<6;y++){
 			for(var x=0;x<8;x++){
 				ctx.fillStyle="#fff";
-				
-				//if( Math.pow(-1, x+y) == -1 ) ctx.fillStyle="#ccc";
-				//else ctx.fillStyle="#eee";
 
-				//ctx.fillRect(x*60,y*60,60,60);
-
-				ctx.strokeStyle = '#ddd';
+				ctx.strokeStyle = "rgba(0, 0, 0, 0.1)";;
 
 				line(x*60, y*60, x*60, y*60+10);
 				line(x*60, y*60, x*60+10, y*60);
@@ -215,7 +399,56 @@ window.addEventListener("load", function(){ //SE EJECUTA DESPUES DE CARGAR TODO
 				line(x*60+60, y*60+60, x*60+60, y*60+50);
 			}
 		}
+
+		if(changeCell){
+			ctx.strokeStyle = '#f00';
+			ctx.rect(changeCell.x*60, changeCell.y*60, 60, 60);
+			ctx.stroke();
+		}
 		
+	}
+
+	checkImageData = function(){
+		console.log(originalData);
+		console.log(puzzleData);
+		if(w){
+			w.postMessage({original:originalData, puzzle:puzzleData});
+		}
+	}
+
+	if(w){
+		w.onmessage = function(event){
+			console.log(event.data);
+			var wrongPieces = event.data;
+			var ctx = canvas.getContext("2d");
+			ctx.strokeStyle = "rgba(255, 0, 0, 0.3)";
+			for(var i in wrongPieces){
+				var x = wrongPieces[i].x;
+				var y = wrongPieces[i].y;
+				ctx.lineWidth = 5;
+				line(x*60+10, y*60+10, x*60+50, y*60+50);
+				line(x*60+10, y*60+50, x*60+50, y*60+10);
+			}
+			if(wrongPieces.length == 0){
+				toggleWin();
+			}
+		}
+	}
+
+	toggleLoading = function(){
+		document.getElementById("loadingdiv").classList.toggle("show");
+	}
+
+	toggleWin = function(){
+		document.getElementById("windiv").classList.toggle("show");
+		document.getElementById("counterwin").innerHTML = movements;
+	}
+
+	rePlay = function(){
+		toggleWin();
+		resetPuzzle();	
+		updateCanvas();
+		populatePieces();
 	}
 	
 
